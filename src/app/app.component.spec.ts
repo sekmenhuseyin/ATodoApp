4b63c0ea9b890e364214ea31709f7d04116ed6ac
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2019-08-23 12:49:26 +0300

    app updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
-      ],
+        RouterTestingModule,
+        FormsModule
+  ],
       declarations: [
         AppComponent
       ],
@@ -18,18 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ATodoApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ATodoApp');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ATodoApp!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { TodoDataService } from './Todos/todo-data.service';
 import { Component } from '@angular/core';
+import { Todo } from './Todos/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'ATodoApp';
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  newTodo: Todo = new Todo(null);
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo: Todo) {
+    this.todoDataService.toggle(todo.id);
+  }
+
+  addTodo() {
+    this.todoDataService.add(this.newTodo);
+    this.newTodo = new Todo(null);
+  }
+
+  removeTodo(todo: Todo) {
+    this.todoDataService.delete(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAll();
+  }
 }
